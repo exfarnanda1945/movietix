52e8b97ab62fc06adc3e796be1d7181e0a191cf5
miko <exfarnanda1945@gmail.com>
2024-07-27 12:55:26 +0700

    add domain exception mapping

--- app/src/main/java/com/exfarnanda1945/movietix/core/domain/DomainException.kt
+++ app/src/main/java/com/exfarnanda1945/movietix/core/domain/DomainException.kt
@@ -1,5 +1,11 @@
 package com.exfarnanda1945.movietix.core.domain
 
+import com.exfarnanda1945.movietix.core.repository.BadRequestException
+import com.exfarnanda1945.movietix.core.repository.ConnectivityErrorException
+import com.exfarnanda1945.movietix.core.repository.InternalServerException
+import com.exfarnanda1945.movietix.core.repository.NotFoundException
+import com.exfarnanda1945.movietix.core.repository.UnExpectedErrorException
+import com.exfarnanda1945.movietix.core.repository.UnauthorizedException
 import java.lang.Exception
 
 
@@ -9,3 +15,13 @@ class InternalServerError : Exception()
 class NotFoundError : Exception()
 class ConnectivityError : Exception()
 class UnExpectedError : Exception()
+
+fun domainExceptionMapping(exception: Exception) = when (exception) {
+    is UnauthorizedException -> UnauthorizedError()
+    is ConnectivityErrorException -> ConnectivityError()
+    is BadRequestException -> BadRequestError()
+    is NotFoundException -> NotFoundError()
+    is InternalServerException -> InternalServerError()
+    is UnExpectedErrorException -> UnExpectedError()
+    else -> UnExpectedError()
+}
