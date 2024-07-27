ee975ec1770d73963e2bb634b514d96492435956
miko <exfarnanda1945@gmail.com>
2024-07-28 00:51:10 +0700

    remove unused code

--- app/src/main/java/com/exfarnanda1945/movietix/home/banner/presentation/BannerScreen.kt
+++ app/src/main/java/com/exfarnanda1945/movietix/home/banner/presentation/BannerScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
