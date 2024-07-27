931874542f5590ef767af5fb527dece0edd0a754
miko <exfarnanda1945@gmail.com>
2024-07-28 00:49:51 +0700

    add constant file

---
+++ app/src/main/java/com/exfarnanda1945/movietix/core/Constants.kt
@@ -0,0 +1,9 @@
+package com.exfarnanda1945.movietix.core
+
+object Constants {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+    const val TOKEN =
+        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMzY3MzAyZDk5Njg1MzkyOWE0MDdhZTVhOWMyODIwYiIsIm5iZiI6MTcyMjAwMDI4NS4xMTAzNDYsInN1YiI6IjY2YTNhMjExNjFkOTMyODljNzE5MDc4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.26lclw6QE60G7t5uOlVfwW0f5wH1xYWpQZf35Czem_8"
+
+    const val BASE_URL_IMAGE ="https://image.tmdb.org/t/p/w500"
+}
