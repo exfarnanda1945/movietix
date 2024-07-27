b3229160900ae510822b9486c01f6860092cb857
miko <exfarnanda1945@gmail.com>
2024-07-27 11:11:55 +0700

    refactor after moving error and exception to core also using result class core

--- app/src/main/java/com/exfarnanda1945/movietix/home/banner/repository/GetBannerUseCaseImpl.kt
+++ app/src/main/java/com/exfarnanda1945/movietix/home/banner/repository/GetBannerUseCaseImpl.kt
@@ -1,14 +1,22 @@
 package com.exfarnanda1945.movietix.home.banner.repository
 
-import com.exfarnanda1945.movietix.home.banner.domain.BadRequestError
+import com.exfarnanda1945.movietix.core.domain.BadRequestError
+import com.exfarnanda1945.movietix.core.domain.ConnectivityError
+import com.exfarnanda1945.movietix.core.domain.DomainResult
+import com.exfarnanda1945.movietix.core.domain.InternalServerError
+import com.exfarnanda1945.movietix.core.domain.NotFoundError
+import com.exfarnanda1945.movietix.core.domain.UnExpectedError
+import com.exfarnanda1945.movietix.core.domain.UnauthorizedError
+import com.exfarnanda1945.movietix.core.repository.BadRequestException
+import com.exfarnanda1945.movietix.core.repository.ConnectivityErrorException
+import com.exfarnanda1945.movietix.core.repository.InternalServerException
+import com.exfarnanda1945.movietix.core.repository.NotFoundException
+import com.exfarnanda1945.movietix.core.repository.RepositoryResult
+import com.exfarnanda1945.movietix.core.repository.UnExpectedErrorException
+import com.exfarnanda1945.movietix.core.repository.UnauthorizedException
 import com.exfarnanda1945.movietix.home.banner.domain.Banner
 import com.exfarnanda1945.movietix.home.banner.domain.BannerResult
-import com.exfarnanda1945.movietix.home.banner.domain.ConnectivityError
 import com.exfarnanda1945.movietix.home.banner.domain.GetBannerUseCase
-import com.exfarnanda1945.movietix.home.banner.domain.InternalServerError
-import com.exfarnanda1945.movietix.home.banner.domain.NotFoundError
-import com.exfarnanda1945.movietix.home.banner.domain.UnExpectedError
-import com.exfarnanda1945.movietix.home.banner.domain.UnauthorizedError
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
@@ -16,21 +24,21 @@ class GetBannerUseCaseImpl(private val remote: GetBannerRemoteRepository) : GetB
     override fun get(): Flow<BannerResult> = flow {
         remote.get().collect { result ->
             when (result) {
-                is BannerRemoteResult.Failure -> {
+                is RepositoryResult.Exception -> {
                     when (result.exception) {
-                        is UnauthorizedException -> emit(BannerResult.Failure(UnauthorizedError()))
-                        is ConnectivityErrorException -> emit(BannerResult.Failure(ConnectivityError()))
-                        is BadRequestException -> emit(BannerResult.Failure(BadRequestError()))
-                        is NotFoundException -> emit(BannerResult.Failure(NotFoundError()))
-                        is InternalServerException -> emit(BannerResult.Failure(InternalServerError()))
-                        is UnExpectedErrorException -> emit(BannerResult.Failure(UnExpectedError()))
+                        is UnauthorizedException -> emit(DomainResult.Error(UnauthorizedError()))
+                        is ConnectivityErrorException -> emit(DomainResult.Error(ConnectivityError()))
+                        is BadRequestException -> emit(DomainResult.Error(BadRequestError()))
+                        is NotFoundException -> emit(DomainResult.Error(NotFoundError()))
+                        is InternalServerException -> emit(DomainResult.Error(InternalServerError()))
+                        is UnExpectedErrorException -> emit(DomainResult.Error(UnExpectedError()))
                     }
                 }
 
-                is BannerRemoteResult.Success -> emit(
-                    BannerResult.Success(
-                        result.banner.map { it.toResult() })
-                )
+                is RepositoryResult.Success -> {
+                    val mapToResult = result.data.map { it.toResult() }
+                    emit(DomainResult.Success(mapToResult))
+                }
             }
         }
     }
--- app/src/main/java/com/exfarnanda1945/movietix/home/banner/service/GetBannerRemoteRepositoryImpl.kt
+++ app/src/main/java/com/exfarnanda1945/movietix/home/banner/service/GetBannerRemoteRepositoryImpl.kt
@@ -1,15 +1,16 @@
 package com.exfarnanda1945.movietix.home.banner.service
 
 import android.util.Log
-import com.exfarnanda1945.movietix.home.banner.repository.BadRequestException
+import com.exfarnanda1945.movietix.core.repository.BadRequestException
+import com.exfarnanda1945.movietix.core.repository.ConnectivityErrorException
+import com.exfarnanda1945.movietix.core.repository.InternalServerException
+import com.exfarnanda1945.movietix.core.repository.NotFoundException
+import com.exfarnanda1945.movietix.core.repository.RepositoryResult
+import com.exfarnanda1945.movietix.core.repository.UnExpectedErrorException
+import com.exfarnanda1945.movietix.core.repository.UnauthorizedException
 import com.exfarnanda1945.movietix.home.banner.repository.BannerRemote
 import com.exfarnanda1945.movietix.home.banner.repository.BannerRemoteResult
-import com.exfarnanda1945.movietix.home.banner.repository.ConnectivityErrorException
 import com.exfarnanda1945.movietix.home.banner.repository.GetBannerRemoteRepository
-import com.exfarnanda1945.movietix.home.banner.repository.InternalServerException
-import com.exfarnanda1945.movietix.home.banner.repository.NotFoundException
-import com.exfarnanda1945.movietix.home.banner.repository.UnExpectedErrorException
-import com.exfarnanda1945.movietix.home.banner.repository.UnauthorizedException
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import retrofit2.HttpException
@@ -20,22 +21,23 @@ class GetBannerRemoteRepositoryImpl(private val service: GetBannerService) :
     override fun get(): Flow<BannerRemoteResult> = flow {
         try {
             val result = service.getBanner()
-            emit(BannerRemoteResult.Success(result.bannerResultItemResponses.map { it.toRemoteResult() }))
+            emit(RepositoryResult.Success(result.bannerResultItemResponses.map { it.toRemoteResult() }))
         } catch (e: Exception) {
             when (e) {
-                is IOException -> emit(BannerRemoteResult.Failure(ConnectivityErrorException()))
+                is IOException -> emit(RepositoryResult.Exception(ConnectivityErrorException()))
                 is HttpException -> {
                     when (e.code()) {
-                        401 -> emit(BannerRemoteResult.Failure(UnauthorizedException()))
-                        400 -> emit(BannerRemoteResult.Failure(BadRequestException()))
-                        404 -> emit(BannerRemoteResult.Failure(NotFoundException()))
-                        500 -> emit(BannerRemoteResult.Failure(InternalServerException()))
+                        401 -> emit(RepositoryResult.Exception(UnauthorizedException()))
+                        400 -> emit(RepositoryResult.Exception(BadRequestException()))
+                        404 -> emit(RepositoryResult.Exception(NotFoundException()))
+                        500 -> emit(RepositoryResult.Exception(InternalServerException()))
                     }
                 }
+
                 else -> {
                     e.printStackTrace()
                     Log.e("getBannerServiceError", "get: ${e.message}")
-                    emit(BannerRemoteResult.Failure(UnExpectedErrorException()))
+                    emit(RepositoryResult.Exception(UnExpectedErrorException()))
                 }
             }
         }
