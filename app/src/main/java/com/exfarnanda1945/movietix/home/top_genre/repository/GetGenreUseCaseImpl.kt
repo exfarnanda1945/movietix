2f082d326e8418f5c8a8cacfaae479a06c8ab952
miko <exfarnanda1945@gmail.com>
2024-07-27 12:58:34 +0700

    refactor genre domain and repository exception mapper

--- app/src/main/java/com/exfarnanda1945/movietix/home/top_genre/repository/GetGenreUseCaseImpl.kt
+++ app/src/main/java/com/exfarnanda1945/movietix/home/top_genre/repository/GetGenreUseCaseImpl.kt
@@ -1,17 +1,8 @@
 package com.exfarnanda1945.movietix.home.top_genre.repository
 
-import com.exfarnanda1945.movietix.home.banner.domain.BadRequestError
-import com.exfarnanda1945.movietix.home.banner.domain.ConnectivityError
-import com.exfarnanda1945.movietix.home.banner.domain.InternalServerError
-import com.exfarnanda1945.movietix.home.banner.domain.NotFoundError
-import com.exfarnanda1945.movietix.home.banner.domain.UnExpectedError
-import com.exfarnanda1945.movietix.home.banner.domain.UnauthorizedError
-import com.exfarnanda1945.movietix.home.banner.repository.BadRequestException
-import com.exfarnanda1945.movietix.home.banner.repository.ConnectivityErrorException
-import com.exfarnanda1945.movietix.home.banner.repository.InternalServerException
-import com.exfarnanda1945.movietix.home.banner.repository.NotFoundException
-import com.exfarnanda1945.movietix.home.banner.repository.UnExpectedErrorException
-import com.exfarnanda1945.movietix.home.banner.repository.UnauthorizedException
+import com.exfarnanda1945.movietix.core.domain.DomainResult
+import com.exfarnanda1945.movietix.core.domain.domainExceptionMapper
+import com.exfarnanda1945.movietix.core.repository.RepositoryResult
 import com.exfarnanda1945.movietix.home.top_genre.domain.Genre
 import com.exfarnanda1945.movietix.home.top_genre.domain.GenreResult
 import com.exfarnanda1945.movietix.home.top_genre.domain.GetGenresUseCase
@@ -22,18 +13,14 @@ class GetGenreUseCaseImpl(private val remote: GetGenreRemoteRepository) : GetGen
     override fun get(): Flow<GenreResult> = flow {
         remote.get().collect { result ->
             when (result) {
-                is GenreRemoteResult.Failure -> when (result.exception) {
-                    is UnauthorizedException -> emit(GenreResult.Failure(UnauthorizedError()))
-                    is ConnectivityErrorException -> emit(GenreResult.Failure(ConnectivityError()))
-                    is BadRequestException -> emit(GenreResult.Failure(BadRequestError()))
-                    is NotFoundException -> emit(GenreResult.Failure(NotFoundError()))
-                    is InternalServerException -> emit(GenreResult.Failure(InternalServerError()))
-                    is UnExpectedErrorException -> emit(GenreResult.Failure(UnExpectedError()))
+                is RepositoryResult.Failure -> {
+                    val exception = domainExceptionMapper(exception = result.exception)
+                    emit(DomainResult.Error(exception))
                 }
 
-                is GenreRemoteResult.Success -> {
-                    val mapToResult = result.genres.map { it.toResult() }
-                    emit(GenreResult.Success(mapToResult))
+                is RepositoryResult.Success -> {
+                    val mapToResult = result.data.map { it.toResult() }
+                    emit(DomainResult.Success(mapToResult))
                 }
             }
         }
--- app/src/main/java/com/exfarnanda1945/movietix/home/top_genre/service/GetGenreRemoteRepositoryImpl.kt
+++ app/src/main/java/com/exfarnanda1945/movietix/home/top_genre/service/GetGenreRemoteRepositoryImpl.kt
@@ -1,46 +1,26 @@
 package com.exfarnanda1945.movietix.home.top_genre.service
 
-import android.util.Log
-import com.exfarnanda1945.movietix.home.banner.repository.BadRequestException
-import com.exfarnanda1945.movietix.home.banner.repository.ConnectivityErrorException
-import com.exfarnanda1945.movietix.home.banner.repository.InternalServerException
-import com.exfarnanda1945.movietix.home.banner.repository.NotFoundException
-import com.exfarnanda1945.movietix.home.banner.repository.UnExpectedErrorException
-import com.exfarnanda1945.movietix.home.banner.repository.UnauthorizedException
+import com.exfarnanda1945.movietix.core.repository.RepositoryResult
+import com.exfarnanda1945.movietix.core.repository.repositoryExceptionMapper
 import com.exfarnanda1945.movietix.home.top_genre.repository.GenreRemote
 import com.exfarnanda1945.movietix.home.top_genre.repository.GenreRemoteResult
 import com.exfarnanda1945.movietix.home.top_genre.repository.GetGenreRemoteRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import retrofit2.HttpException
-import java.io.IOException
 
-class GetGenreRemoteRepositoryImpl(private val service: GetGenreService) : GetGenreRemoteRepository {
+class GetGenreRemoteRepositoryImpl(private val service: GetGenreService) :
+    GetGenreRemoteRepository {
     override fun get(): Flow<GenreRemoteResult> = flow {
         try {
             val result = service.get()
-            emit(GenreRemoteResult.Success(
+            emit(RepositoryResult.Success(
                 result.genres.map { it.toRemoteResult() }
             ))
 
         } catch (e: Exception) {
-            when (e) {
-                is IOException -> emit(GenreRemoteResult.Failure(ConnectivityErrorException()))
-                is HttpException -> {
-                    when (e.code()) {
-                        401 -> emit(GenreRemoteResult.Failure(UnauthorizedException()))
-                        400 -> emit(GenreRemoteResult.Failure(BadRequestException()))
-                        404 -> emit(GenreRemoteResult.Failure(NotFoundException()))
-                        500 -> emit(GenreRemoteResult.Failure(InternalServerException()))
-                    }
-                }
-
-                else -> {
-                    e.printStackTrace()
-                    Log.e("getGenreServiceError", "get: ${e.message}")
-                    emit(GenreRemoteResult.Failure(UnExpectedErrorException()))
-                }
-            }
+            val result =
+                repositoryExceptionMapper(exception = e, tagException = "GetGenreRemoteRepository")
+            emit(RepositoryResult.Failure(result))
         }
     }
 
