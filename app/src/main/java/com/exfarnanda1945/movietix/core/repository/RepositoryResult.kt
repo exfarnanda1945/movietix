89876726d75caae91569f18aa74e24effc79b519
miko <exfarnanda1945@gmail.com>
2024-07-27 12:55:48 +0700

    refactor result class to repository result

--- app/src/main/java/com/exfarnanda1945/movietix/core/repository/RepositoryResult.kt
+++ app/src/main/java/com/exfarnanda1945/movietix/core/repository/RepositoryResult.kt
@@ -2,5 +2,5 @@ package com.exfarnanda1945.movietix.core.repository
 
 sealed class RepositoryResult<out T, out E> {
     data class Success<T>(val data: T) : RepositoryResult<T, Nothing>()
-    data class Exception<E>(val exception: E) : RepositoryResult<Nothing, E>()
+    data class Failure<E>(val exception: E) : RepositoryResult<Nothing, E>()
 }
--- app/src/main/java/com/exfarnanda1945/movietix/home/top_genre/repository/GenreRemote.kt
+++ app/src/main/java/com/exfarnanda1945/movietix/home/top_genre/repository/GenreRemote.kt
@@ -1,11 +1,10 @@
 package com.exfarnanda1945.movietix.home.top_genre.repository
 
+import com.exfarnanda1945.movietix.core.repository.RepositoryResult
+
 data class GenreRemote(
     val id: Int,
     val name: String
 )
 
-sealed class GenreRemoteResult {
-    data class Success(val genres: List<GenreRemote>) : GenreRemoteResult()
-    data class Failure(val exception: Exception) : GenreRemoteResult()
-}
+typealias GenreRemoteResult = RepositoryResult<List<GenreRemote>,Exception>
