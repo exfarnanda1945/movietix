348563692a39601cedc1265067fe992e9254ad74
miko <exfarnanda1945@gmail.com>
2024-07-28 01:27:06 +0700

    implement top genre screen view model

--- app/src/main/java/com/exfarnanda1945/movietix/home/top_genre/presentation/TopGenreScreen.kt
+++ app/src/main/java/com/exfarnanda1945/movietix/home/top_genre/presentation/TopGenreScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
@@ -16,36 +17,50 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import org.koin.androidx.compose.koinViewModel
+import org.koin.compose.KoinContext
 
 @Composable
 fun TopGenreScreen(modifier: Modifier = Modifier) {
-    Column(modifier) {
-        Text(
-            text = "Top Categories", style = TextStyle(
-                fontSize = 16.sp, fontWeight = FontWeight.Bold
-            ), modifier = Modifier.padding(start = 10.dp, bottom = 10.dp)
-        )
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 10.dp),
-            horizontalArrangement = Arrangement.spacedBy(10.dp)
-        ) {
-            Column(
-                modifier = Modifier.weight(1f),
-                verticalArrangement = Arrangement.spacedBy(10.dp)
-            ) {
-                GenreCard("Action")
-                GenreCard("Super Heroes")
-            }
-            Column(
-                modifier = Modifier.weight(1f),
-                verticalArrangement = Arrangement.spacedBy(10.dp)
-            ) {
-                GenreCard("Documentary")
-                GenreCard("Comedy")
-            }
+    val genreViewModel = koinViewModel<TopGenreScreenViewModel>()
+    val state by genreViewModel.genreState.collectAsStateWithLifecycle()
 
+    KoinContext {
+        Column(modifier) {
+            Text(
+                text = "Top Genre For You", style = TextStyle(
+                    fontSize = 16.sp, fontWeight = FontWeight.Bold
+                ), modifier = Modifier.padding(start = 10.dp, bottom = 10.dp)
+            )
+            if (state.isLoading) {
+                Text(text = "Loading...")
+            } else {
+                val genre = state.data
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 10.dp),
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    Column(
+                        modifier = Modifier.weight(1f),
+                        verticalArrangement = Arrangement.spacedBy(10.dp)
+                    ) {
+                        GenreCard(genre[0].name)
+                        GenreCard(genre[1].name)
+                    }
+                    Column(
+                        modifier = Modifier.weight(1f),
+                        verticalArrangement = Arrangement.spacedBy(10.dp)
+                    ) {
+                        GenreCard(genre[2].name)
+                        GenreCard(genre[3].name)
+                    }
+
+                }
+            }
         }
     }
 }
