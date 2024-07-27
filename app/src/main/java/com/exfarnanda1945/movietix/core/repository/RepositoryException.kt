cc558154b72aadb48d8a9bb0e8780622c7daf583
miko <exfarnanda1945@gmail.com>
2024-07-27 11:10:39 +0700

    add repository exception

---
+++ app/src/main/java/com/exfarnanda1945/movietix/core/repository/RepositoryException.kt
@@ -0,0 +1,8 @@
+package com.exfarnanda1945.movietix.core.repository
+
+class UnauthorizedException:Exception()
+class BadRequestException:Exception()
+class InternalServerException:Exception()
+class NotFoundException:Exception()
+class ConnectivityErrorException:Exception()
+class UnExpectedErrorException:Exception()
