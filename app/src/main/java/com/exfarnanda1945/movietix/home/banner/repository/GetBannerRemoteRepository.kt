da460328f1356af037a0129318c3467747267af4
miko <exfarnanda1945@gmail.com>
2024-07-27 11:10:23 +0700

    move exception to repository exception

--- app/src/main/java/com/exfarnanda1945/movietix/home/banner/repository/GetBannerRemoteRepository.kt
+++ app/src/main/java/com/exfarnanda1945/movietix/home/banner/repository/GetBannerRemoteRepository.kt
@@ -6,11 +6,6 @@ interface GetBannerRemoteRepository {
     fun get(): Flow<BannerRemoteResult>
 }
 
-class UnauthorizedException:Exception()
-class BadRequestException:Exception()
-class InternalServerException:Exception()
-class NotFoundException:Exception()
-class ConnectivityErrorException:Exception()
-class UnExpectedErrorException:Exception()
+
 
 
