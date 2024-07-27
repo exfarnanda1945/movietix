2cb7a6ba445b4aef273b84c4c020848c0733c8bb
miko <exfarnanda1945@gmail.com>
2024-07-27 12:57:06 +0700

    add repository exception mapper

--- app/src/main/java/com/exfarnanda1945/movietix/core/repository/RepositoryException.kt
+++ app/src/main/java/com/exfarnanda1945/movietix/core/repository/RepositoryException.kt
@@ -1,8 +1,30 @@
 package com.exfarnanda1945.movietix.core.repository
 
-class UnauthorizedException:Exception()
-class BadRequestException:Exception()
-class InternalServerException:Exception()
-class NotFoundException:Exception()
-class ConnectivityErrorException:Exception()
-class UnExpectedErrorException:Exception()
+import android.util.Log
+import retrofit2.HttpException
+import java.io.IOException
+
+class UnauthorizedException : Exception()
+class BadRequestException : Exception()
+class InternalServerException : Exception()
+class NotFoundException : Exception()
+class ConnectivityErrorException : Exception()
+class UnExpectedErrorException : Exception()
+
+fun repositoryExceptionMapper(exception: Exception,tagException:String) = when (exception) {
+    is IOException -> ConnectivityErrorException()
+    is HttpException -> {
+        when (exception.code()) {
+            401 -> UnauthorizedException()
+            400 -> BadRequestException()
+            404 -> NotFoundException()
+            500 -> InternalServerException()
+            else -> UnExpectedErrorException()
+        }
+    }
+    else -> {
+        exception.printStackTrace()
+        Log.e(tagException, "exception: ${exception.message}")
+        UnExpectedErrorException()
+    }
+}
