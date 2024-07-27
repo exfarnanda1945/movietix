fc37cfd58e42da2d7ff588477de24a6acb7f308d
miko <exfarnanda1945@gmail.com>
2024-07-27 23:01:22 +0700

    add origin country param and default value params service

--- app/src/main/java/com/exfarnanda1945/movietix/home/shared_discovery_film/service/GetDiscoveryFilmService.kt
+++ app/src/main/java/com/exfarnanda1945/movietix/home/shared_discovery_film/service/GetDiscoveryFilmService.kt
@@ -6,11 +6,12 @@ import retrofit2.http.Query
 interface GetDiscoveryFilmService {
     @GET("discover/movie/")
     fun get(
-        @Query("with_genres") genreId: Int,
         @Query("include_adult") includeAdult: Boolean = false,
         @Query("include_video") includeVideo: Boolean = false,
         @Query("language") language: String = "en-US",
         @Query("page") page: Int = 1,
-        @Query("sort_by") sortBy: String = "popularity.desc"
+        @Query("sort_by") sortBy: String = "popularity.desc",
+        @Query("with_genres") genreId: Int? = null,
+        @Query("with_origin_country") originCountry:String? = null
     ): DiscoveryFilmResponse
 }
