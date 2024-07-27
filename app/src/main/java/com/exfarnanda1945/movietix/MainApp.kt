4fd66c845cd471117998a135d52178cf93384a99
miko <exfarnanda1945@gmail.com>
2024-07-28 00:48:15 +0700

    add MainApp to initialize koin

---
+++ app/src/main/java/com/exfarnanda1945/movietix/MainApp.kt
@@ -0,0 +1,20 @@
+package com.exfarnanda1945.movietix
+
+import android.app.Application
+import com.exfarnanda1945.movietix.core.di.httpModule
+import com.exfarnanda1945.movietix.home.banner.di.bannerModule
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MainApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            modules(
+                httpModule,
+                bannerModule
+            )
+        }
+    }
+}
