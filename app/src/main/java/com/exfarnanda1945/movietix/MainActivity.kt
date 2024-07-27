ffe8faa41730d3e42f49508cf00c8b0d2ee5abdf
miko <exfarnanda1945@gmail.com>
2024-07-28 00:49:19 +0700

    add KoinAndroidContext

--- app/src/main/java/com/exfarnanda1945/movietix/MainActivity.kt
+++ app/src/main/java/com/exfarnanda1945/movietix/MainActivity.kt
@@ -4,16 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import com.exfarnanda1945.movietix.home.HomeScreen
 import com.exfarnanda1945.movietix.ui.theme.MovieTixTheme
+import org.koin.androidx.compose.KoinAndroidContext
+import org.koin.core.annotation.KoinExperimentalAPI
 
 class MainActivity : ComponentActivity() {
+    @OptIn(KoinExperimentalAPI::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MovieTixTheme {
-                HomeScreen()
+            KoinAndroidContext {
+                MovieTixTheme {
+                    HomeScreen()
+                }
             }
         }
     }
