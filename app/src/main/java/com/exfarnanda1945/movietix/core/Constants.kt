4284ce879842b9797cf6ee7e349a7ce2547d216a
miko <exfarnanda1945@gmail.com>
2024-07-28 18:22:56 +0700

    add base url video yt

--- app/src/main/java/com/exfarnanda1945/movietix/core/Constants.kt
+++ app/src/main/java/com/exfarnanda1945/movietix/core/Constants.kt
@@ -6,4 +6,5 @@ object Constants {
         "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMzY3MzAyZDk5Njg1MzkyOWE0MDdhZTVhOWMyODIwYiIsIm5iZiI6MTcyMjAwMDI4NS4xMTAzNDYsInN1YiI6IjY2YTNhMjExNjFkOTMyODljNzE5MDc4MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.26lclw6QE60G7t5uOlVfwW0f5wH1xYWpQZf35Czem_8"
 
     const val BASE_URL_IMAGE ="https://image.tmdb.org/t/p/w500"
+    const val BASE_URL_VIDEO ="https://www.youtube.com/watch?v="
 }
